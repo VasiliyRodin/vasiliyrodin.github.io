1abb123a12fc5252a1ba57cd0a8713d3f00c1ee2
Vasiliy Rodin <vrodin@Vasiliys-MacBook-Pro.local>
2015-06-02 15:05:23 -0700

    test

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.getElementById(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.getElementById(".randomPizzaContainer")[i], size);
-      var newwidth = (document.getElementById(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.getElementById(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
